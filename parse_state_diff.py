356a5e4813ce13df590496c345a64e2e0388f979
vishnu Prasad Chegondi <vishnu.chegondi@gmail.com>
2024-03-17 17:37:30 +0530

    Use Strip()

--- parse_state_diff.py
+++ parse_state_diff.py
@@ -5,9 +5,9 @@ def diff_state_file(plan_output, plan_output_2):
     if len(plan_output)-len(plan_output_2) > 0:
         for line in plan_output_2.split("\n"):
             if line in plan_output:
-                diff_output=diff_output.trim()+line
+                diff_output=diff_output+"\n"+line
     else:
         for line in plan_output.split("\n"):
             if line in plan_output_2:
-                diff_output=diff_output.trim()+line
-    return diff_output
+                diff_output=diff_output+"\n"+line
+    return diff_output.strip()
